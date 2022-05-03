2318de5e36fd183f6b49a0f9202fdcbd0d7d963c
dhkang <soejdtodxn@naver.com>
2022-05-04 01:16:17 +0900

    Initial solution commit

---
+++ baekjoon_algorithm/baekjoon_algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace baekjoon_algorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var input = Console.ReadLine().Split();
+            var input = File.ReadAllText(@"../../../input.txt");
+
+            File.WriteAllText(@"../../../output.txt", $""); 
+            //Console.WriteLine($"{h} {w}");
+        }
+    }
+}
