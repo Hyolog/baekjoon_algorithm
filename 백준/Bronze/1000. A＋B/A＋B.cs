bfbe1e0bce9c17f1ca632106ffae2f908ec6f6e7
Hyolo <soejdtodxn@gmail.com>
2022-05-04 00:52:45 +0900

    [Bronze V] Title: A+B, Time: 200 ms, Memory: 4684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Program {
+    public static void Main() {
+            var input = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
+    }
+}
