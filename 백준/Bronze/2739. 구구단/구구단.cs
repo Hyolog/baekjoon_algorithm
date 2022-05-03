5fed9debc33db1dd9ccc481fa59ca1514d78fdfd
Hyolo <soejdtodxn@gmail.com>
2022-05-04 00:40:21 +0900

    [Bronze III] Title: 구구단, Time: 68 ms, Memory: 5788 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+                Console.WriteLine($"{num} * {i} = {num * i}");
+        }
+    }
+}
